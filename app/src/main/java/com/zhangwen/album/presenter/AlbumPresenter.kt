093f58830958c98e025069bca3cd53073841e1ca
zhangwen <zhangwen.wendyys@bytedance.com>
2020-11-13 18:33:10 +0800

    添加资源文件

--- app/src/main/java/com/zhangwen/album/presenter/AlbumPresenter.kt
+++ app/src/main/java/com/zhangwen/album/presenter/AlbumPresenter.kt
@@ -2,6 +2,7 @@ package com.zhangwen.album.presenter
 
 import android.content.Context
 import android.os.Message
+import android.widget.Toast
 import com.bytedance.common.utility.collection.WeakHandler
 import com.zhangwen.album.Utils.ImageLoader
 import com.zhangwen.album.view.AlbumView
@@ -18,6 +19,8 @@ class AlbumPresenter(var context: Context) : BasePresenter<AlbumView>(),WeakHand
         photoList = ImageLoader.getImageList(context.contentResolver)
         if(photoList.size>0){
             mView?.updateAlbumPhoto(photoList)
+        }else{
+            Toast.makeText(context,"没有图片",Toast.LENGTH_SHORT).show()
         }
     }
 
