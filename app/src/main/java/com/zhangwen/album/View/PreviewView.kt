bb10461cdab647bc12a1815d60535d3d2f539fdd
zhangwen <zhangwen.wendyys@bytedance.com>
2020-11-24 14:59:18 +0800

    optimize

--- app/src/main/java/com/zhangwen/album/View/PreviewView.kt
+++ app/src/main/java/com/zhangwen/album/View/PreviewView.kt
@@ -1,7 +1,10 @@
 package com.zhangwen.album.View
 
 interface PreviewView : BaseView {
+
     fun updateCurrentPage(pos: Int)
+
     fun updateToggleButtonState(checked: Boolean)
+
     fun updateThumbListState(count: Int)
 }
